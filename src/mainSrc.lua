a0d540625f1d60e297551149d9db08cce7473633
kura <89491373+kuraise@users.noreply.github.com>
2022-07-01 13:52:06 -0400

    Update mainSrc.lua

--- src/mainSrc.lua
+++ src/mainSrc.lua
@@ -1,7 +1,8 @@
 local rbxmSuite = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/richie0866/rbxm-suite/master/src/rbxm-suite.lua"))()
 local RoseRel = rbxmSuite.download("AlpineSoftworks/Rose@latest", "mainSource.rbxm")
 local GET = rbxmSuite.launch(RoseRel)
-GET.Parent = game.CoreGui.RobloxGui
+print(GET.Parent)
+GET.Parent = game.CoreGui
 local src = rbxmSuite.require(GET.src)
 
 return src
